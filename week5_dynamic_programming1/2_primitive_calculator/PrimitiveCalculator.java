fa823c7f1bc84b7c34b55130692f8e459bd15d7d
qiz <qi.zhang04@sap.com>
2023-04-23 10:26:06 -0700

    primitive calculator

--- week5_dynamic_programming1/2_primitive_calculator/PrimitiveCalculator.java
+++ week5_dynamic_programming1/2_primitive_calculator/PrimitiveCalculator.java
@@ -1,41 +1,50 @@
 import java.util.*;
 
 public class PrimitiveCalculator {
-    private static List<Integer> optimal_sequence(int n) {
-        List<Integer>[] sequence = new LinkedList[n + 1];
-        for (int i = 0; i < sequence.length; i++) {
-            sequence[i] = new LinkedList<>();
-        }
-        int[] dp = new int[n + 1];
-        dp[1] = 0;
-        sequence[1].add(1);
-        for (int i = 2; i < dp.length; i++) {
-            int n1 = dp[i - 1] + 1;
-            int n2 = (i % 2 == 0 && i / 2 >= 0) ? dp[i / 2] + 1 : Integer.MAX_VALUE;
-            int n3 = (i % 3 == 0 && i / 3 >= 0) ? dp[i / 3] + 1 :  Integer.MAX_VALUE;
 
-            dp[i] = Math.min(Math.min(n1, n2), n3);
-            if ( dp[i] == n3) {
-                sequence[i].addAll(sequence[i / 3]);
-                sequence[i].add(3);
-            } else if (dp[i] == n2){
-                sequence[i].addAll(sequence[i / 2]);
-                sequence[i].add(2);
+    public static class Entry {
+        public int cost;
+        public int prev;
+        public int op;
+        public Entry(int cost, int prev, int op) {
+            this.cost = cost;
+            this.prev = prev;
+            this.op = op;
+        }
+    }
+    public static List<Integer> optimal_sequence(int n) {
+        Entry[] dp = new Entry[n]; // number value is i + 1, right shift by one
+        dp[0] = new Entry(0, 0, 0);
+        for (int i = 1; i < n; i++) {
+            int cost1 = dp[i - 1].cost + 1;
+            int cost2 = (i + 1) % 2 == 0 ? dp[(i + 1) / 2 - 1].cost + 1 : Integer.MAX_VALUE;
+            int cost3 = (i + 1) % 3 == 0 ? dp[(i + 1) / 3 - 1].cost + 1 : Integer.MAX_VALUE;
+
+            int minCost = Math.min(Math.min(cost1, cost2), cost3);
+            if (minCost == cost1) {
+                dp[i] = new Entry(cost1, i - 1, 1 );
+            } else if (minCost == cost2) {
+                dp[i] = new Entry(cost2, (i + 1) / 2 - 1, 2 );
             } else {
-                sequence[i].addAll(sequence[i - 1]);
-                sequence[i].add(1);
+                dp[i] = new Entry(cost3, (i + 1) / 3 - 1, 3 );
             }
-
         }
-        //System.out.println(sequence[i].toString());
-        return sequence[n];
+
+        ArrayList<Integer> result = new ArrayList<>();
+        int index = n - 1;
+        while (index != 0) {
+            result.add(dp[index].op);
+            index = dp[index].prev;
+        }
+        Collections.reverse(result);
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         List<Integer> sequence = optimal_sequence(n);
-        System.out.println(sequence.size() - 1);
+        System.out.println(sequence.size());
         for (Integer x : sequence) {
             System.out.print(x + " ");
         }
